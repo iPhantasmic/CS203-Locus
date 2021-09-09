584a488d1855ce528752bb30f66cdb418c6f100c
Justin <justin.100600@gmail.com>
2021-09-09 12:22:40 +0800

    Create Example Project Structure

---
+++ src/main/java/com/cs203/creditswees/models/user/User.java
@@ -0,0 +1,10 @@
+package com.cs203.creditswees.models.user;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
