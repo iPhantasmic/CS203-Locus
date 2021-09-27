a78b5c08c65d7be79fc5d86e8cdb17824064a259
alvinowyong <70066269+alvinowyong@users.noreply.github.com>
2021-09-27 22:54:58 +0800

    BE: CorsFilter

--- src/main/java/com/cs203/locus/config/WebConfig.java
+++ src/main/java/com/cs203/locus/config/WebConfig.java
@@ -1,7 +1,15 @@
 package com.cs203.locus.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebMvc
@@ -17,6 +25,21 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**").allowedOrigins(url)
                 .allowedMethods("GET", "POST","PUT", "DELETE", "OPTIONS");
     }
+
+    /* Block of code*/
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/v2/api-docs", "/v2/api-docs");
