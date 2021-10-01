67975b2bc3db57ef491eb23e8ef9e34a5f70e2ec
Oversparkling <nicholasongck@gmail.com>
2021-10-02 02:04:13 +0800

    Update Navbar.js

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,54 +1,79 @@
 import { useState, useEffect } from "react";
-import { Menu, Image } from "antd";
+import { Menu, Image, Divider } from "antd";
 import {
     MailOutlined,
     AppstoreOutlined,
     SettingOutlined,
 } from "@ant-design/icons";
 import { Header } from "antd/lib/layout/layout";
-import cartoon from "../public/logo.png";
 
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-            <Menu
-                selectedKeys={["mail"]}
-                mode="horizontal"
-                className="items-center w-full flex flex-row justify-between"
-            >
-                <div>
-                    <Image src={cartoon} height = {300} width = {300}/>
-                </div>
-                <Menu.Item key="mail" icon={<MailOutlined />}>
-                    Navigation One
-                </Menu.Item>
-                <Menu.Item key="app" icon={<AppstoreOutlined />}>
-                    Navigation Two
-                </Menu.Item>
-                {/* <SubMenu
-                    key="SubMenu"
-                    icon={<SettingOutlined />}
-                    title="Navigation Three - Submenu"
-                >
-                    <Menu.ItemGroup title="Item 1">
-                        <Menu.Item key="setting:1">Option 1</Menu.Item>
-                        <Menu.Item key="setting:2">Option 2</Menu.Item>
-                    </Menu.ItemGroup>
-                    <Menu.ItemGroup title="Item 2">
-                        <Menu.Item key="setting:3">Option 3</Menu.Item>
-                        <Menu.Item key="setting:4">Option 4</Menu.Item>
-                    </Menu.ItemGroup>
-                </SubMenu> */}
-                <Menu.Item key="alipay">
+        // <Menu
+        //     selectedKeys={["mail"]}
+        //     mode="horizontal"
+        //     className="items-center w-full flex flex-row"
+        // >
+        //     <Menu.Item>
+        //         <Image src = "./logo.png" />
+        //     </Menu.Item>
+
+        //     <Menu.Item key="mail" icon={<MailOutlined />}>
+        //         Navigation One
+        //     </Menu.Item>
+        //     <Menu.Item key="app" icon={<AppstoreOutlined />}>
+        //         Navigation Two
+        //     </Menu.Item>
+        //     <SubMenu
+        //         key="SubMenu"
+        //         icon={<SettingOutlined />}
+        //         title="Navigation Three - Submenu"
+        //     >
+        //         <Menu.ItemGroup title="Item 1">
+        //             <Menu.Item key="setting:1">Option 1</Menu.Item>
+        //             <Menu.Item key="setting:2">Option 2</Menu.Item>
+        //         </Menu.ItemGroup>
+        //         <Menu.ItemGroup title="Item 2">
+        //             <Menu.Item key="setting:3">Option 3</Menu.Item>
+        //             <Menu.Item key="setting:4">Option 4</Menu.Item>
+        //         </Menu.ItemGroup>
+        //     </SubMenu>
+        //     <Menu.Item key="alipay">
+        //         <a
+        //             href="https://ant.design"
+        //             target="_blank"
+        //             rel="noopener noreferrer"
+        //         >
+        //             Navigation Four - Link
+        //         </a>
+        //     </Menu.Item>
+        // </Menu>
+        <div className="h-16 w-full px-7 bg-black flex flex-row items-center justify-between">
+            <Image src="./locus_new_logo_white.png" height={80} width={110} />
+            <div className="flex-row flex h-full items-center">
+                <div className={`hover:underline flex-col flex h-full justify-center px-8 ${props.page == 'Home' ? "bg-gray-800 " : ""}`}>
                     <a
-                        href="https://ant.design"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                        className="text-white"
+                        style={{ textDecoration: "none" }}
+                        href="/login"
                     >
-                        Navigation Four - Link
+                        Home
                     </a>
-                </Menu.Item>
-            </Menu>
-        
+                </div>
+                <div className={`hover:underline flex-col flex h-full justify-center px-8 ${props.page == 'Events' ? "bg-gray-800 " : ""}`}>
+                    <a className="text-white">Events</a>
+                </div>
+                <div className={`hover:underline flex-col flex h-full justify-center px-8 ${props.page == 'Tickets' ? "bg-gray-800 " : ""}`}>
+                    <a className="text-white">Tickets</a>
+                </div>
+                <div className={`hover:underline flex-col flex h-full justify-center px-8 ${props.page == 'Resources' ? "bg-gray-800 " : ""}`}>
+                    <a className="text-white">Resources</a>
+                </div>
+            </div>
+            <div>
+                <span className="text-white">God of Jye-Ra</span>
+            </div>
+        </div>
     );
 }
