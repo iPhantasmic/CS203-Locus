76830133ffef0b09cdd9fcf147c61ee7cb56a6c6
alvinowyong <70066269+alvinowyong@users.noreply.github.com>
2021-10-12 19:38:02 +0800

    Disable CV

--- src/main/java/com/cs203/locus/swagger/SpringFoxConfig.java
+++ src/main/java/com/cs203/locus/swagger/SpringFoxConfig.java
@@ -1,44 +1,44 @@
-//package com.cs203.locus.swagger;
-//
-//import java.util.Arrays;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.ApiKey;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SpringFoxConfig {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo())
-//                .securitySchemes(Arrays.asList(apiKey()));
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("CS203 creditSWEEs REST API Document")
-//                .description("work in progress")
-//                .termsOfServiceUrl("localhost")
-//                .version("1.0")
-//                .build();
-//    }
-//
-//    private ApiKey apiKey() {
-//        return new ApiKey("jwtToken", "Authorization", "header");
-//    }
-//
-//}
+package com.cs203.locus.swagger;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("CS203 creditSWEEs REST API Document")
+                .description("work in progress")
+                .termsOfServiceUrl("localhost")
+                .version("1.0")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("jwtToken", "Authorization", "header");
+    }
+
+}
