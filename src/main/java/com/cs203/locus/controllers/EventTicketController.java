cb913cd126a077bbe573bbdba93381b720fc19ed
Justin <justin.100600@gmail.com>
2021-10-17 17:41:16 +0800

    EventTicketServiceTest

--- src/main/java/com/cs203/locus/controllers/EventTicketController.java
+++ src/main/java/com/cs203/locus/controllers/EventTicketController.java
@@ -2,15 +2,20 @@ package com.cs203.locus.controllers;
 
 import java.util.ArrayList;
 
+import com.cs203.locus.models.event.Event;
 import com.cs203.locus.models.event.EventTicket;
 import com.cs203.locus.models.event.EventTicketDTO;
+import com.cs203.locus.models.participant.Participant;
 import com.cs203.locus.service.EventService;
 import com.cs203.locus.service.EventTicketService;
 
 import com.cs203.locus.service.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 //TODO: Remaining PUT method
@@ -54,6 +59,12 @@ public class EventTicketController {
     @GetMapping("/{id}")
     public @ResponseBody ResponseEntity<EventTicketDTO> findById(@PathVariable Integer id){
         EventTicket result = eventTicketService.findById(id);
+
+        if (result == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "No EventTicket with ID: " + id);
+        }
+
         EventTicketDTO toRet = new EventTicketDTO();
         toRet.setParticipantName(result.getParticipant().getUser().getName());
         toRet.setOrganiserName(result.getEvent().getOrganiser().getUser().getName());
@@ -69,6 +80,11 @@ public class EventTicketController {
     // TODO: ensure participant can only access his own list of EventTickets
     @GetMapping(value ="/listParticipantTickets/{id}")
     public @ResponseBody ResponseEntity<ArrayList<EventTicketDTO>> getParticipantTickets(@PathVariable Integer id){
+        if (participantService.findById(id) == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "No Participant with ID: " + id);
+        }
+
         Iterable<EventTicket> temp = eventTicketService.findEventTicketByParticipant(id);
         ArrayList<EventTicketDTO> result = new ArrayList<>();
         for (EventTicket eventTicket : temp) {
@@ -91,18 +107,47 @@ public class EventTicketController {
 
     // TODO: ensure only participant can create an EventTicket for himself
     @PostMapping("/new")
-    public EventTicket addTicket(@RequestParam Integer participantId, @RequestParam Integer eventId) {
+    public @ResponseBody ResponseEntity<EventTicketDTO> addTicket(@RequestParam Integer participantId, @RequestParam Integer eventId) {
+        Event event = eventService.findById(eventId);
+        if (event == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "No Event with ID: " + eventId);
+        }
+
+        Participant participant = participantService.findById(participantId);
+        if (participant == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "No Participant with ID: " + eventId);
+        }
+
         EventTicket ticket = new EventTicket();
-        ticket.setEvent(eventService.findById(eventId));
+        ticket.setEvent(event);
         ticket.setParticipant(participantService.findById(participantId));
 
-        return eventTicketService.addTicket(ticket);
+        EventTicket created = eventTicketService.addTicket(ticket);
+        EventTicketDTO toRet = new EventTicketDTO();
+        toRet.setId(created.getId());
+        toRet.setParticipantName(created.getParticipant().getUser().getName());
+        toRet.setParticipantId(created.getParticipant().getId());
+        toRet.setOrganiserName(created.getEvent().getOrganiser().getUser().getName());
+        toRet.setOrganiserId(created.getEvent().getOrganiser().getId());
+        toRet.setEventName(created.getEvent().getName());
+        toRet.setEventId(created.getEvent().getId());
+        toRet.setStartDateTime(created.getEvent().getStartDateTime());
+        toRet.setEndDateTime(created.getEvent().getEndDateTime());
+        toRet.setEventAddress(created.getEvent().getAddress());
+
+        return ResponseEntity.ok(toRet);
     }
 
     // TODO: ensure only participant can delete own EventTicket
     @DeleteMapping("/{id}")
     public @ResponseBody ResponseEntity<EventTicket> deleteWithId(@PathVariable Integer id) {
         EventTicket result = eventTicketService.deleteById(id);
+        if (result == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "No EventTicket with ID: " + id);
+        }
 
         return ResponseEntity.ok(result);
     }
---
+++ src/test/java/com/cs203/locus/EventTicketServiceTest.java
@@ -0,0 +1,150 @@
+package com.cs203.locus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import com.cs203.locus.models.event.Event;
+import com.cs203.locus.models.event.EventTicket;
+import com.cs203.locus.models.organiser.Organiser;
+import com.cs203.locus.models.participant.Participant;
+import com.cs203.locus.repository.EventTicketRepository;
+import com.cs203.locus.service.EventTicketService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Optional;
+
+
+@ExtendWith(MockitoExtension.class)
+public class EventTicketServiceTest {
+
+    @Mock
+    private EventTicketRepository eventTickets;
+
+    @InjectMocks
+    private EventTicketService eventTicketService;
+
+
+    @Test
+    void getAllEventTickets_Success_ReturnAllEventTickets() {
+        // mock the "findAll" operation
+        when(eventTickets.findAll()).thenReturn(new ArrayList<EventTicket>());
+
+        // act
+        Iterable<EventTicket> returnedList = eventTicketService.findAll();
+
+        // assert
+        assertNotNull(returnedList);
+        verify(eventTickets).findAll();
+    }
+
+    @Test
+    void getEventTicket_Success_ReturnEventTicket() {
+        // arrange
+        EventTicket ticket = new EventTicket();
+        ticket.setId(1);
+        ticket.setEvent(new Event());
+        ticket.setParticipant(new Participant());
+
+        // mock
+        when(eventTickets.findById(1)).thenReturn(Optional.of(ticket));
+
+        // act
+        EventTicket result = eventTicketService.findById(1);
+
+        // assert
+        assertNotNull(result);
+        assertEquals(ticket,result);
+        verify(eventTickets).findById(ticket.getId());
+    }
+
+    @Test
+    void getEventTicket_InvalidTicketId_ReturnNull() {
+        // mock
+        when(eventTickets.findById(123)).thenReturn(Optional.empty());
+
+        // act
+        EventTicket result = eventTicketService.findById(123);
+
+        // assert
+        assertNull(result);
+        verify(eventTickets).findById(123);
+    }
+
+    @Test
+    void addEventTicket_Success_ReturnCreatedEventTicket() {
+        // arrange
+        EventTicket ticket = new EventTicket();
+        ticket.setId(1);
+        ticket.setParticipant(new Participant());
+        ticket.setEvent(new Event());
+
+        // mock
+        when(eventTickets.save(any(EventTicket.class))).thenReturn(ticket);
+
+        // act
+        EventTicket savedTicket = eventTicketService.addTicket(ticket);
+
+        // assert
+        assertEquals(ticket, savedTicket);
+        verify(eventTickets).save(ticket);
+    }
+
+    @Test
+    void getEventTicketForUser_Success_ReturnEventTicketList() {
+        // mock the "findAll" operation
+        when(eventTickets.findByParticipantId(1)).thenReturn(new ArrayList<EventTicket>());
+
+        // act
+        Iterable<EventTicket> returnedList = eventTicketService.findEventTicketByParticipant(1);
+
+        // assert
+        assertNotNull(returnedList);
+        verify(eventTickets).findByParticipantId(1);
+    }
+
+    @Test
+    void deleteEventTicket_Success_ReturnDeletedEventTicket() {
+        // arrange
+        EventTicket ticket = new EventTicket();
+        ticket.setEvent(new Event());
+        ticket.setId(1);
+        ticket.setParticipant(new Participant());
+
+        Integer id = 1;
+        // mock
+        when(eventTickets.findById(id)).thenReturn(Optional.of(ticket));
+
+        // act
+        EventTicket deleted = eventTicketService.deleteById(id);
+
+        // assert
+        assertNotNull(deleted);
+        assertEquals(ticket, deleted);
+        verify(eventTickets).findById(id);
+        verify(eventTickets).delete(ticket);
+    }
+
+    @Test
+    void deleteEventTicket_InvalidEventTicketId_ReturnDeletedEventTicket() {
+        // mock
+        Integer id = 1;
+        when(eventTickets.findById(id)).thenReturn(Optional.empty());
+
+        // act
+        EventTicket deleted = eventTicketService.deleteById(id);
+
+        // assert
+        assertNull(deleted);
+        verify(eventTickets, never()).deleteById(id);
+    }
+}
