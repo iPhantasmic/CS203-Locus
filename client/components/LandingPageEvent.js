4a50e7e474e2db8fda1a612e96f4386384008426
alvinowyong <70066269+alvinowyong@users.noreply.github.com>
2021-11-08 20:19:49 +0800

    FE: GeoCoding SideScroll

--- client/components/LandingPageEvent.js
+++ client/components/LandingPageEvent.js
@@ -23,34 +23,5 @@ export default function EventCard(props) {
                 </div>
             </div>
         </Slide>
-        // <div
-        //     className="flex-row flex p-5 items-center rounded-xl mb-5 shadow"
-        //     style={{ height: 180,width:'48%' }}
-        // >
-        //     <div className=" flex-col flex h-full ml-5 justify-between p-1" style = {{width:'40%'}}>
-        //         <span className="font-bold" style={{ fontSize: 20 }}>
-        //             {/* 3D Augmented Chalk Exhibit */}
-        //             {props.title}
-        //         </span>
-        //         <span style={{ color: "#32BEA6", fontSize: 13 }}>
-        //             {/* Fri, 17th Sep 2021, 1:00 PM */}
-        //             {props.dateTime}
-        //         </span>
-        //         <span>
-        //             {/* Gardens by the Bay • Singapore */}
-        //             {props.location}
-        //         </span>
-        //         {/* <span className = "font-bold">Secret Sunrise Singapore Pte. Ltd</span> */}
-        //         <span className = "font-bold">{props.tag}</span>
-        //     </div>
-        //     <div>
-        //         <img
-        //             src="https://picsum.photos/250/150"
-        //             height={150}
-        //             width={270}
-        //             className="rounded-xl"
-        //         />
-        //     </div>
-        // </div>
     );
 }
