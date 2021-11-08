e3f7540bef50c991d789b1bdc63d811d3b073d57
Justin <justin.100600@gmail.com>
2021-11-09 01:49:02 +0800

    Implement Admin News Management Endpoint

---
+++ src/main/java/com/cs203/locus/models/admin/NewsArticleDTO.java
@@ -0,0 +1,36 @@
+package com.cs203.locus.models.admin;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class NewsArticleDTO implements Serializable {
+
+    private String title;
+
+    private String bodyText;
+
+    private LocalDateTime datePublished;
+
+    private String articleLink;
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getBodyText() { return bodyText; }
+    public void setBodyText(String bodyText) { this.bodyText = bodyText; }
+
+    public LocalDateTime getDatePublished() { return datePublished; }
+    public void setDatePublished(LocalDateTime datePublished) { this.datePublished = datePublished; }
+
+    public String getArticleLink() { return articleLink; }
+    public void setArticleLink(String articleLink) { this.articleLink = articleLink; }
+
+    public String toJson() {
+        return "{" +
+                "\"title\":\"" + title + "\"," +
+                "\"bodyText\":\"" + bodyText + "\"," +
+                "\"datePublished\":\"" + datePublished.toString() + "\"," +
+                "\"articleLink\":\"" + articleLink + "\"}";
+    }
+    
+}
