a97dcae85374d0391d27834d2bf6884ade9b7437
Justin <justin.100600@gmail.com>
2021-11-15 14:16:15 +0800

    OrganiserIntegrationTest Refactoring

--- src/test/java/com/cs203/locus/integration/OrganiserIntegrationTest.java
+++ src/test/java/com/cs203/locus/integration/OrganiserIntegrationTest.java
@@ -1,28 +1,29 @@
 package com.cs203.locus.integration;
 
+import com.cs203.locus.models.organiser.Organiser;
+import com.cs203.locus.models.organiser.OrganiserDTO;
 import com.cs203.locus.models.user.User;
 import com.cs203.locus.models.user.UserDTO;
 import com.cs203.locus.repository.OrganiserRepository;
-import com.cs203.locus.repository.ParticipantRepository;
 import com.cs203.locus.repository.UserRepository;
 import com.cs203.locus.security.JwtUserDetailsService;
 import com.cs203.locus.service.OrganiserService;
-import com.cs203.locus.service.ParticipantService;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrganiserIntegrationTest {
     @LocalServerPort
     private int port;
@@ -53,8 +54,8 @@ public class OrganiserIntegrationTest {
     }
 
 
-    @BeforeEach
-    void setupUser() {
+    @BeforeAll
+    void setupUser(@Autowired JwtUserDetailsService jwtUserDetailsService) {
         UserDTO userDTO = new UserDTO();
         userDTO.setName("testAccount");
         userDTO.setUsername("testAccount");
@@ -62,63 +63,84 @@ public class OrganiserIntegrationTest {
         userDTO.setPassword("P@ssw0rd");
         userDTO.setConfirmPassword("P@ssw0rd");
         user = jwtUserDetailsService.create(userDTO);
+
         getToken(userDTO.getUsername(), userDTO.getPassword());
     }
 
-    @AfterEach
-    void tearDown() {
+    @AfterAll
+    void tearDown(@Autowired UserRepository userRepository, @Autowired OrganiserRepository organiserRepository) {
         userRepository.deleteAll();
+        organiserRepository.deleteAll();
     }
 
     @Test
+    @Order(1)
     public void getOrganiser_ValidId_200() {
-        Integer organiserId = user.getOrganiserProfile().getId();
+        int organiserId = user.getOrganiserProfile().getId();
         given()
-                .header("Authorization", "Bearer " + jwtToken)
-                .when()
-                .get(baseUrl + port + "/organiser/" + organiserId)
-                .then()
-                .assertThat()
-                .statusCode(200);
+            .header("Authorization", "Bearer " + jwtToken)
+        .when()
+            .get(baseUrl + port + "/organiser/" + organiserId)
+        .then()
+            .assertThat()
+            .statusCode(200);
     }
+
     @Test
-    public void getOrganiser_InvalidId_400() {
-        Integer organiserId = user.getOrganiserProfile().getId()+1;
+    @Order(2)
+    public void getOrganiser_InvalidId_403() {
+        int organiserId = user.getOrganiserProfile().getId() + 1;
         given()
-                .header("Authorization", "Bearer " + jwtToken)
-                .when()
-                .get(baseUrl + port + "/organiser/" + organiserId)
-                .then()
-                .assertThat()
-                .statusCode(400);
+            .header("Authorization", "Bearer " + jwtToken)
+        .when()
+            .get(baseUrl + port + "/organiser/" + organiserId)
+        .then()
+            .assertThat()
+            .statusCode(403);
     }
-    @Test
-    public void updateOrganiser_ValidId_200() {
-        Integer organiserId = user.getOrganiserProfile().getId();
 
-        RequestSpecification requestSpec = RestAssured.with();
-        requestSpec.given().contentType("application/json");
-        requestSpec.body("{\"id\":\"" + organiserId + "\",\"companyName\": " + "\"Hello\""+",\"companySector\":" +"\"Hello\"" + " ,\"companyAcra\":" + "\"Hello\" }");
-        requestSpec
-                .header("Authorization", "Bearer " + jwtToken)
-                .put(baseUrl + port + "/organiser/"+organiserId)
-                .then()
-                .assertThat()
-                .statusCode(200);
+    @Test
+    @Order(3)
+    public void updateOrganiser_ValidIdValidOrganiserDTO_200() {
+        Organiser organiser = user.getOrganiserProfile();
+        OrganiserDTO organiserDTO = new OrganiserDTO();
+        organiserDTO.setId(organiser.getId());
+        organiserDTO.setCompanyAcra(organiserDTO.getCompanyAcra());
+        organiserDTO.setCompanyName("New Company Name");
+        organiserDTO.setCompanySector(organiserDTO.getCompanySector());
+
+        given()
+            .contentType("application/json")
+            .header("Authorization", "Bearer " + jwtToken)
+            .body(organiserDTO)
+        .when()
+            .put(baseUrl + port + "/organiser/" + organiser.getId())
+        .then()
+            .assertThat()
+            .statusCode(200)
+            .body("companyName", equalTo("New Company Name"));
     }
-    @Test
-    public void updateOrganiser_InvalidId_400() {
-        Integer organiserId = user.getOrganiserProfile().getId()+1;
 
-        RequestSpecification requestSpec = RestAssured.with();
-        requestSpec.given().contentType("application/json");
-        requestSpec.body("{\"id\":\"" + organiserId + "\",\"companyName\": " + "\"Hello\""+",\"companySector\":" +"\"Hello\"" + " ,\"companyAcra\":" + "\"Hello\" }");
-        requestSpec
-                .header("Authorization", "Bearer " + jwtToken)
-                .put(baseUrl + port + "/organiser/"+organiserId)
-                .then()
-                .assertThat()
-                .statusCode(400);
+    @Test
+    @Order(4)
+    public void updateOrganiser_InvalidId_403() {
+        int organiserId = user.getOrganiserProfile().getId() + 1;
+        Organiser organiser = user.getOrganiserProfile();
+        OrganiserDTO organiserDTO = new OrganiserDTO();
+        organiserDTO.setId(organiser.getId());
+        organiserDTO.setCompanyAcra(organiserDTO.getCompanyAcra());
+        organiserDTO.setCompanyName("New Company Name");
+        organiserDTO.setCompanySector(organiserDTO.getCompanySector());
+
+        given()
+            .contentType("application/json")
+            .header("Authorization", "Bearer " + jwtToken)
+            .body(organiserDTO)
+        .when()
+            .put(baseUrl + port + "/organiser/" + organiserId)
+        .then()
+            .assertThat()
+            .statusCode(403);
     }
 
 }
