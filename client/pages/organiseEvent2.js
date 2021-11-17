b1680b1bd0c9cf1ff69d6415c41f4db3ae9444af
alvinowyong <70066269+alvinowyong@users.noreply.github.com>
2021-11-17 15:32:32 +0800

    BE: EventTicket Fixes

--- client/pages/organiseEvent2.js
+++ client/pages/organiseEvent2.js
@@ -81,6 +81,7 @@ export default function Home() {
             })
             .catch(function (error) {
                 console.log(error);
+                setLoading(false);
             });
     };
 
